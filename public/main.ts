177c314683255fe5d11cfbdbb02b1ec3b7ebbe9a
Okiki <okikio.dev@gmail.com>
2021-07-13 22:59:58 +0000

    push update

--- public/main.ts
+++ public/main.ts
@@ -1,13 +1,15 @@
 import { boot } from "delicate-lib";
-import { ANIMATE, ApplyCustomEasing } from "complex-lib";
+import { ANIMATE, ApplyCustomEasing, CustomEasing } from "complex-lib";
+
 boot();
 
 ANIMATE.updateOptions({
     target: "#animated-box",
+
+    translateX: CustomEasing([0, 600]),
     ...ApplyCustomEasing({
-        translateX: [0, 500],
-        translateY: [100, 500],
-        easing: "spring"
+        translateY: [100, 400],
+        easing: "out-bounce"
     }),
     duration: 3000,
     loop: true,
--- src/pages/index.md
+++ src/pages/index.md
@@ -5,7 +5,7 @@ layout: ../layouts/Main.astro
 
 <img src="https://github.com/snowpackjs/astro/blob/main/assets/social/banner.png?raw=true" alt="Astro" width="638" height="320" >
 
-## What is Astro?
+## Pssh....What is Astro?
 
 **Astro** is a _fresh but familiar_ approach to building websites. Astro combines decades of proven performance best practices with the DX improvements of the component-oriented era.
 
